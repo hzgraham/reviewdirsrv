0b108b4d647b3dd722e17dc2a5adfefbe4bc86fd
Henry Graham <hgraham@redhat.com>
2014-01-12 12:09:12 -0500

    initial package files

---
+++ README.txt
@@ -0,0 +1,4 @@
+reviewdirsrv
+============
+
+analyze directory server data
---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='ReviewDirsrv',
+    version='0.1dev',
+    packages=['reviewdirsrv',],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.txt').read(),
+)
+
