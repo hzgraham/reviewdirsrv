f847cfa4ee067b2ae69c6d199b5282bfd559e523
Henry Graham <hgraham@redhat.com>
2014-01-18 17:27:41 -0500

    starting the arguement parsing

--- reviewdirsrv/Parser.py
+++ reviewdirsrv/Parser.py
@@ -1,42 +1,22 @@
 #!/usr/bin/env python
 # Copyright (c) 2014 Henry Graham <hgraham@redhat.com>
 #
-#
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-
+#
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import sys, os, subprocess
-import reviewdirsrv.analyze
+import argparse
 
-"""
-need to differentiate between IPA and a stand alone 389 Directory Server
-"""
-
-'''
-if len(args) != 1:
-    parser.error("Incorrect number of arguments")
-else:
-
-pathname = sys.arg[1]
-print pathname
-'''
-
-def usage():
-    print """Usage: sumarize [--init, -i|--check, -ch|--clean, -cl]
-    --init, -i          
-
-    Examples:
-
-       Initialize:
-
-"""
+class Parser()
+    def __init__(self)
+        self.parser = argparse.ArguementParser(description='analyse sosreport')
+        self.parser.add_arguements('--path', help='path to extracted sosreport')
--- reviewdirsrv/analyze.py
+++ reviewdirsrv/analyze.py
@@ -1,22 +1,20 @@
 #!/usr/bin/env python
 # Copyright (c) 2014 Henry Graham <hgraham@redhat.com>
 #
-#
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-
+#
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import sys, os, subprocess
-import reviewdirsrv.analyze
 
 """
 need to differentiate between IPA and a stand alone 389 Directory Server
@@ -40,3 +38,35 @@ def usage():
        Initialize:
 
 """
+class analyze:
+
+    def __init__(self):
+        
+        self.hostname = self.readhostname() 
+        variables = analyze()
+        self.dirsrvpath = /etc/dirsrv
+
+    def _env_settings()
+        
+
+    def readhostname():
+
+        hostname = os.path.join(self.pathname,'hostname')
+        hstnm = open('hostname', 'r')
+        name = hstnm.read()
+        print "the hostname is = %s" % name
+
+    def check_logs()
+       subprocess.cal(["logconv.pl","-V","" % instance
+       logs = subprocess.Popen(["logvonv.pl", "-V", "/etc/dirsrv/%s" % instance ], stdout=subprocess.PIPE)
+       output,  = logs.communicate()
+       return logs
+
+    def get_instance()                   
+        """need to determine the instance name either through input or code"""
+        os.path
+        for each in d = listdir(dirsvpath
+        glob.glob   
+        isdir(join(dirsrvpath,d))
+
+
